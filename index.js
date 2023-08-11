2e9ea41896ad3a77f2a72771dbabf02f78dbb383
lemiascode <107190544+lemiascode@users.noreply.github.com>
2023-08-11 19:22:02 +0000

    last commit

--- index.js
+++ index.js
@@ -7,9 +7,12 @@
 // }
 function gen_text() {
   const citation = ["La plus grande gloire n'est pas de ne jamais tomber, mais de se relever à chaque chute.", "Aucun de nous, en agissant seul, ne peut atteindre le succès.", "Un gagnant est juste un rêveur qui n'a jamais cédé."];
-  const text = Math.floor(Math.random() * citation.length);
-  const index = citation[text];
+  setInterval(() => {
+    const text = Math.floor(Math.random() * citation.length);
+    const index = citation[text];
+    console.log(citation[text]);
+  }, 3000);
+//last commit
 
-  console.log(citation[text]);
 }
 gen_text();
