75057d6f6c2ffafdd633a74e3fb59365194dc5f2
lemiascode <107190544+lemiascode@users.noreply.github.com>
2023-08-21 18:24:36 +0000

    "I changed the math.floo method by integer then I changed the name of the gen_text function by gen_citation"

--- index.js
+++ index.js
@@ -5,14 +5,14 @@
 //   console.log(citation[index]);
 
 // }
-function gen_text() {
+function gen_citation() {
   const citation = ["La plus grande gloire n'est pas de ne jamais tomber, mais de se relever à chaque chute.", "Aucun de nous, en agissant seul, ne peut atteindre le succès.", "Un gagnant est juste un rêveur qui n'a jamais cédé."];
   setInterval(() => {
-    const text = Math.floor(Math.random() * citation.length);
-    const index = citation[text];
-    console.log(citation[text]);
+    const index = (Math.random() * citation.length | 0) ;
+  
+    console.log(citation[index]);
   }, 3000);
 //last commit
 
 }
-gen_text();
+gen_citation();
